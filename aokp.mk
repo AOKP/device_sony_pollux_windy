1fbd26ee0bd8ce02f26efe8ec9cdc6b0805be0e4
Arnav Gupta <championswimmer@aokp.co>
2013-12-06 10:17:51 +0530

    pollux_windy: update AOKP build configuration
    Change-Id: Iacd73239277b9d95411fe7df73957cf5ca8294dd
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- aokp.mk
+++ aokp.mk
@@ -4,27 +4,7 @@ $(call inherit-product, vendor/aokp/configs/common_tablet.mk)
 # Inherit AOSP device configuration for anzu
 $(call inherit-product-if-exists, device/sony/pollux_windy/full_pollux_windy.mk)
 
-# Setup device specific product configuration.
-PRODUCT_NAME := aokp_polluxwindy
-PRODUCT_BRAND := Sony
-PRODUCT_DEVICE := pollux_windy
-PRODUCT_MODEL := Xperia Tablet Z WiFi
-PRODUCT_MANUFACTURER := Sony
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP311 BUILD_FINGERPRINT=Sony/SGP311_1272-1907/SGP311:4.1.2/10.1.C.0.344/p35_rw:user/release-keys PRIVATE_BUILD_DESC="SGP311-user 4.1.2 10.1.C.0.344 p35_rw test-keys"
 
 #skip asserts for now
 TARGET_OTA_ASSERT_SKIP := true
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP311 BUILD_FINGERPRINT=Sony/SGP311_1272-1907/SGP311:4.1.2/10.1.C.0.344/p35_rw:user/release-keys PRIVATE_BUILD_DESC="SGP311-user 4.1.2 10.1.C.0.344 p35_rw test-keys"
-
-# Camera
-PRODUCT_PACKAGES += \
-    Camera \
-    Mms \
-    Music
-
-# Copy anzu specific prebuilt files
-PRODUCT_PACKAGES += \
-    ThinkFree  
-
-PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
